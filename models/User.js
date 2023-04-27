7d6b4816fd80fab24bbb829d2897a8ff3c496c34
Ash <acst52@gmail.com>
2023-04-27 15:13:21 -0400

    remove comment

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 // Schema to create User model
 const userSchema = new Schema(
-  {
+{
     username: {
       type: String,
       unique: true,
@@ -27,15 +27,15 @@ const userSchema = new Schema(
         ref: 'User',
       },
     ],
-  },
-  {
+},
+{
     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
     // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
     toJSON: {
       virtuals: true,
     },
-    id: false,
-  }
+      id: false,
+}
 );
 
 // Create a virtual property `friendCount` that retrieves the length of the user's friends array field on query
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -18,10 +18,3 @@ router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
-
-// BUG WHEN TRYING TO ADD FRIEND WITH ID - POST req localhost:3001/api/users/6439f239e34d0f7d7e7f7fe7/friends/6439f241e34d0f7d7e7f7fe9 not working in Insomnia. Getting "ImmutableField" error...:
-// {
-//	"ok": 0,
-//	"code": 66,
-//	"codeName": "ImmutableField"
-// }
